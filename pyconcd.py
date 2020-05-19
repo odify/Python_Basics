84a57480f91423cf9b35bc8e06de094c393278d7
odify <odify495@gmail.com>
2020-05-19 03:25:39 +0200

    xyz0z

--- pyconcd.py
+++ pyconcd.py
@@ -1,7 +1,11 @@
-from datetime import datetime
+# from datetime import datetime
 
-PYCON_DATE = datetime(year=2020, month=8, day=28, hour=22)
 
-countdown = PYCON_DATE - datetime.now()
+# ANOTHER_DATE = datetime(year=2020, month=8, day=28, hour=22)
 
-print(f"7 Jahre mit Malina zusammen in: {countdown}")
+
+# countdown = ANOTHER_DATE - datetime.now()
+
+
+# print(f"7 Jahre mit Malina zusammen in: {countdown}")
+
