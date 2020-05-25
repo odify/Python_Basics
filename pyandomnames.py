42fb533ab77ffed3ad60105dc1389b196b39a671
odify <odify495@gmail.com>
2020-05-25 22:00:27 +0200

    pynames.py

--- pyandomnames.py
+++ pyandomnames.py
@@ -1,10 +1,23 @@
-#random names generator
+# 4.pynamegen.py
 
 import random
 
-name = ["Superman","Ironman","Aquaman","Antman","Baman","Thomas","Gui","Micha","Donato","Rahls","Jonas","Momo","Daniel","Kleeberg"]
-index = random.randint(0,len(name))
+ms1 = ["SH4D0W","CHIEF","M4STER","NULL","TOXIC","C4PT41N","EVIL","CYBER","0DARK","ICE","FIRE","PRIV4TE","TRO0L","LIQUID","THE","MAD","ZERO"]
+ms2 = ["BET4","ANGEL","WIRE","B4RC0DE","BOT","HYDRA","NINJ4","NIGHT","DELT4","W1ZZARD","SHARK","SAINT","VIRUZ","DESTR0YER","X","CR4CK","L3G3ND","OMICR0N","PH4NT0M"]
+ms =random.choice(ms1)
+mk =random.choice(ms2)
+mks = (ms + " " + mk)
+i = len(mks)
+def decor(func):
+    def wrap():
+        print("<"*i)
+        func()
+        print(">"*i)
+    return wrap
+def username():
+    print(mks)
+print("Your name is:")
+decorator = decor(username)
+decorator()
 
-a=name[index]
-
-print(a)
+# CHECKOUT YOUR BLACK HAT NAME =)
---
+++ pyname.py
@@ -0,0 +1,10 @@
+# get a random name from array
+
+import random
+
+name = ["Superman","Ironman","Aquaman","Antman","Baman","Thomas","Gui","Micha","Donato","Rahls","Jonas","Momo","Daniel","Kleeberg"]
+index = random.randint(0,len(name))
+
+a=name[index]
+
+print(a)
