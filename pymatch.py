2b3e80b36ef1777aa5151f85501661931d1ebb90
odify <odify495@gmail.com>
2020-05-29 17:15:19 +0200

    ASCII+binäry converter

---
+++ pymatch.py
@@ -0,0 +1,12 @@
+# import re
+
+# pattern = re"python"
+
+# match = re.search(pattern, "pythmonpythlonpythonnohtyp")
+# if match:
+#     print(match.group())
+#     print(match.start())
+#     print(match.end())
+#     print(match.span())
+
+# #UNDER CONSTRUCTION
