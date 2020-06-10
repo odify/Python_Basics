684f4f76139fd7f988c296adebde1d689ebe60b7
odify <odify495@gmail.com>
2020-06-10 20:26:43 +0200

    2020-06-10

---
+++ 2020-06-10/pyXYZ.py
@@ -0,0 +1,24 @@
+# ONLY ANOTHER TESTING FILE
+
+# p = Perceptron()
+# #preparing the training for our little Perceptron 
+# training_set = []
+# for i in range(4500):
+#     x = random.random()*1000
+#     y = random.random()*1000
+#     training_set.append([x, y, answer(x, y)])
+# #a lot of training
+# p.train(training_set)
+
+# #results
+# cont = 0
+# test_set = []
+# for i in range(500):
+#     x = random.random()*1000
+#     y = random.random()*1000
+#     ans = answer(x, y)
+#     result = p.predict([x, y, ans])
+#     if result == ans:
+#         cont+=1
+# #give a % of correct anwser
+# print(str((cont*100)/500) + "%")
