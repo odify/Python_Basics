be4a9b823b4dd9f43ee2d7ec191ccbeadb64c3bf
odify <odify495@gmail.com>
2020-06-26 00:08:18 +0200

    2020-06-25-xyz

---
+++ 2020-06-25/pyvirus01.py
@@ -0,0 +1,19 @@
+# import os
+# indicator = 0
+
+# while True:
+#     with open("virus{0}.py".format(indicator), "w") as f:
+#         f.write(open(__file__).read())
+#         f.close()
+#         indicator += 1
+        
+#         if indicator==1000:
+#             os.fork()
+
+        
+        
+# print("""
+# ...    ...   ......  ...... ......  ........  ......
+#  ...  ...    ... ... ......    .......  ......    ......
+#   ......     ...  ......     ..... ......    ..........
+# """)
