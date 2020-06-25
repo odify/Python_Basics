c4df530cfd6c19a05be4a7f4c5ff27ca8bcaf511
odify <odify495@gmail.com>
2020-06-26 00:04:52 +0200

    2020-06-25

---
+++ 2020-06-25/pynumpyxyz.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Create an array of rank 2
+my_arr = np.array([[1,2,3,4], [5,6,7,8]])
+
+print(my_arr)
+# [[1 2 3 4]
+# [5 6 7 8]]
+
+print(my_arr[1, 2])
+
+print(my_arr.ndim) #ranking...
+
+print(my_arr.shape)
+#( 2, 4)
+
+print(my_arr.size)
+
+print(type(my_arr))
+
+
+
+# # <type 'numpy.ndarray'>
